2074b399b50348e23d82ee26251c51001267b1dc
ngupta <ngupta10.slb@gmail.com>
2023-04-19 16:43:56 +0530

    updated main.go

--- main.go
+++ main.go
@@ -1,23 +1,67 @@
 package main
 
 import (
-	//"net/http"
-	"path/filepath"
+	"context"
+	"fmt"
+	"io"
+	"os"
 
+	"cloud.google.com/go/storage"
 	"github.com/gin-gonic/gin"
 )
+
+func uploadFileToGCS(bucketName, objectName string, r io.Reader) (string, error) {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	defer client.Close()
+
+	bucket := client.Bucket(bucketName)
+	object := bucket.Object(objectName)
+	w := object.NewWriter(ctx)
+	if _, err = io.Copy(w, r); err != nil {
+		return "", err
+	}
+	if err := w.Close(); err != nil {
+		return "", err
+	}
+
+	attrs, err := object.Attrs(ctx)
+	if err != nil {
+		return "", err
+	}
+	return attrs.MediaLink, nil
+}
 
 func main() {
 	r := gin.Default()
 
-	// Set up a route to serve the media files
-	r.GET("/media/:filename", func(c *gin.Context) {
-		filename := c.Param("filename")
-		filePath := filepath.Join("media", filename)
+	r.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Error uploading file: %v", err)
+			return
+		}
 
-		c.File(filePath)
+		f, err := file.Open()
+		if err != nil {
+			c.String(http.StatusBadRequest, "Error opening file: %v", err)
+			return
+		}
+		defer f.Close()
+
+		mediaLink, err := uploadFileToGCS("zicops-vc", file.Filename, f)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Error uploading to GCS: %v", err)
+			return
+		}
+
+		c.String(http.StatusOK, "File uploaded to GCS: %s", mediaLink)
 	})
 
-	// Start the web server on port 8080
 	r.Run(":8080")
 }
+
